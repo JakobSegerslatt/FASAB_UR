d69de32082a0721fcd2ec1a229f1b8bd3544f52a
Jakob Segerslätt <segerslatt@gmail.com>
2016-03-30 17:13:09 +0200

    17.13

--- src/FASAB_UR/ViewModels/UR_fasab.cs
+++ src/FASAB_UR/ViewModels/UR_fasab.cs
@@ -8,8 +8,33 @@ namespace FASAB_UR.ViewModels
 {
     public class UR_fasab
     { // Basformuläret, framtaget av FASAB
-
+        #region Personuppgifter
         [Required]
         public string Name { get; set; }
+
+        public int SSN { get; set; }
+
+        public string Address { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public string City { get; set; }
+
+        public string Email { get; set; }
+
+        public int Phone { get; set; }
+        #endregion
+        #region Övriga uppgifter
+        public string Month { get; set; }
+        public string Region { get; set; }
+
+        public List<Uppdrag> Uppdrag { get; set; }
+
+        public DateTime DateSignedByUser { get; set; }
+        public string SignatureUser { get; set; }
+
+        public DateTime DateSignedByAttester { get; set; }
+        public string SignatureAttester { get; set; }
+        #endregion
     }
 }
---
+++ src/FASAB_UR/ViewModels/Uppdrag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FASAB_UR.ViewModels
+{
+    public class Uppdrag
+    {
+        public DateTime Date { get; set; }
+
+        public string Uppdragsart { get; set; }
+
+        public int WorkedHours { get; set; }
+
+        public int VacationCompPercent { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public int TravelFee { get; set; }
+
+        public int Kostavdrag_antal { get; set; }
+
+        public float CarCompKMTaxfree { get; set; }
+
+        public float CarCompKMTax { get; set; }
+
+        public int OwnExpenses { get; set; }
+
+        public string Vgren { get; set; }
+
+        public string Kställe { get; set; }
+
+        public string Project { get; set; }
+
+
+    }
+}
