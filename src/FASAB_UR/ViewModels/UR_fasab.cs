d050a8c8f5f7584938f54d37d7f5b53482dfb65e
Jakob Segerslätt <segerslatt@gmail.com>
2016-03-30 14:17:34 +0200

    14.17

---
+++ src/FASAB_UR/ViewModels/UR_fasab.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FASAB_UR.ViewModels
+{
+    public class UR_fasab
+    { // Basformuläret, framtaget av FASAB
+
+    }
+}
