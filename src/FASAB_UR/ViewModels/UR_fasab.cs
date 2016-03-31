e79e6f30ebab28d6b3298805687001600ecfc4d7
JonasKlintberg <github.com@czt.se>
2016-03-31 16:10:11 +0200

    Lablar uppdaterade

--- src/FASAB_UR/ViewModels/UR_fasab.cs
+++ src/FASAB_UR/ViewModels/UR_fasab.cs
@@ -10,46 +10,48 @@ namespace FASAB_UR.ViewModels
     { // Basformuläret, framtaget av FASAB
         #region Personuppgifter
         [Required(ErrorMessage = "Du måste skriva in ditt namn")]
+        [Display(Name = "Förnamn och efternamn")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Ange personnummer i formatet ÅÅÅÅMMDDXXXX")]
         [Display(Name = "Personnummer")]
         [Range(12,12, ErrorMessage = "Ange personnummer i formatet ÅÅÅÅMMDDXXXX")]
         public int SSN { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Ange mobiltelefonnummer. Om du saknar mobiltelefon kan du ange ett fast nummmer.")]
+        [Display(Name = "Mobiltelefon")]
         public int Phone { get; set; }
-
-        [Required]
+        [Required(ErrorMessage = "Du måste ange din adress")]
+        [Display(Name = "Gatuadress")]
         public string Address { get; set; }
-        [Required]
-
+        [Required(ErrorMessage = "Du måste ange ditt postnummer")]
+        [Display(Name = "Postnummer")]
         public int ZipCode { get; set; }
-        [Required]
-
+        [Required(ErrorMessage = "Du måste ange din postort")]
+        [Display(Name = "Postort")]
         public string City { get; set; }
-        [Required]
-
+        [Required(ErrorMessage = "Du måste ange din e-postadress")]
+        [Display(Name = "E-postadress")]
         public string Email { get; set; }
 
         #endregion
 
         #region Övriga uppgifter
         [Required]
-
+        //[Display(Name = "Månad")]
         public string Month { get; set; }
         [Required]
-
+        //[Display(Name = "Region")]
         public string Region { get; set; }
         [Required]
-
+        //[Display(Name = "Uppdrag")]
         public List<Uppdrag> Uppdrag { get; set; }
         [Required]
-
+        //[Display(Name = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")]
         public DateTime DateSignedByUser { get; set; }
 
         public string SignatureUser { get; set; }
         [Required]
-
+        //[Display(Name = "Datum för signatur")]
         public DateTime DateSignedByAttester { get; set; }
         public string SignatureAttester { get; set; }
         #endregion
--- src/FASAB_UR/ViewModels/Uppdrag.cs
+++ src/FASAB_UR/ViewModels/Uppdrag.cs
@@ -6,45 +6,51 @@ namespace FASAB_UR.ViewModels
     public class Uppdrag
     {
         [Required(ErrorMessage = "Du har glömt att ange ett datum.")]
+        [Display(Name = "Datum")]
         public DateTime Date { get; set; }
 
         [Required(ErrorMessage = "Du har glömt att ange uppdragets art.")]
+        [Display(Name = "Uppdragsart")]
         public string Uppdragsart { get; set; }
 
         [Required(ErrorMessage = "Du har glömt att ange hur många timmar du arbetat.")]
+        [Display(Name = "Arbetade timmar")]
         public int WorkedHours { get; set; }
 
-        [Required]        
+        [Required(ErrorMessage = "Du måste ange semesterersättning i procent. Ange 0 om semesterersättning ej är applicerbar")]
+        [Display(Name = "Semesterersättning (%)")]
         public int VacationCompPercent { get; set; }
 
         [Required]
+        [Display(Name = "Avresetid")]
         public DateTime StartTime { get; set; }
         [Required]
-
+        [Display(Name = "Ankomsttid")]
         public DateTime EndTime { get; set; }
-        [Required]
 
+        [Required]
+        [Display(Name = "Researvode")]
         public int TravelFee { get; set; }
         [Required]
-
+        [Display(Name = "Kostavdrag, antal")]
         public int Kostavdrag_antal { get; set; }
         [Required]
-
+        [Display(Name = "Bilersättning skattefri (km)")]
         public float CarCompKMTaxfree { get; set; }
         [Required]
-
+        [Display(Name = "Bilersätting skattepliktig (km)")]
         public float CarCompKMTax { get; set; }
         [Required]
-
+        [Display(Name = "Egna utlägg")]
         public int OwnExpenses { get; set; }
         [Required]
-
+        [Display(Name = "Verksamhetsgren")]
         public string Vgren { get; set; }
         [Required]
-
+        [Display(Name = "Kostnadsställe")]
         public string Kställe { get; set; }
         [Required]
-
+        [Display(Name = "Projekt")]
         public string Project { get; set; }
 
 
