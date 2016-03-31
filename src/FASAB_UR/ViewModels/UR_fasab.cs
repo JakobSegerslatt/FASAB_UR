028a69c00e50bdd252e948ab001e1a590144462c
Jakob Segerslätt <segerslatt@gmail.com>
2016-03-31 10:24:15 +0200

    10.24 Class ändringar

--- src/FASAB_UR/ViewModels/UR_fasab.cs
+++ src/FASAB_UR/ViewModels/UR_fasab.cs
@@ -9,29 +9,45 @@ namespace FASAB_UR.ViewModels
     public class UR_fasab
     { // Basformuläret, framtaget av FASAB
         #region Personuppgifter
-        [Required]
+        [Required(ErrorMessage = "Du måste skriva in ditt namn")]
         public string Name { get; set; }
-
+        [Required]
+        
         public int SSN { get; set; }
+        [Required]
 
         public string Address { get; set; }
+        [Required]
 
         public int ZipCode { get; set; }
+        [Required]
 
         public string City { get; set; }
+        [Required]
 
         public string Email { get; set; }
+        [Required]
 
         public int Phone { get; set; }
         #endregion
+
         #region Övriga uppgifter
+        [Required]
+
         public string Month { get; set; }
+        [Required]
+
         public string Region { get; set; }
+        [Required]
 
         public List<Uppdrag> Uppdrag { get; set; }
+        [Required]
 
         public DateTime DateSignedByUser { get; set; }
+        [Required]
+
         public string SignatureUser { get; set; }
+        [Required]
 
         public DateTime DateSignedByAttester { get; set; }
         public string SignatureAttester { get; set; }
--- src/FASAB_UR/ViewModels/Uppdrag.cs
+++ src/FASAB_UR/ViewModels/Uppdrag.cs
@@ -1,33 +1,52 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FASAB_UR.ViewModels
 {
     public class Uppdrag
     {
+        [Required]
+
         public DateTime Date { get; set; }
+
+        [Required]
 
         public string Uppdragsart { get; set; }
 
+        [Required]
+
         public int WorkedHours { get; set; }
+        [Required]
 
         public int VacationCompPercent { get; set; }
+        [Required]
 
         public DateTime StartTime { get; set; }
+        [Required]
+
         public DateTime EndTime { get; set; }
+        [Required]
 
         public int TravelFee { get; set; }
+        [Required]
 
         public int Kostavdrag_antal { get; set; }
+        [Required]
 
         public float CarCompKMTaxfree { get; set; }
+        [Required]
 
         public float CarCompKMTax { get; set; }
+        [Required]
 
         public int OwnExpenses { get; set; }
+        [Required]
 
         public string Vgren { get; set; }
+        [Required]
 
         public string Kställe { get; set; }
+        [Required]
 
         public string Project { get; set; }
 
